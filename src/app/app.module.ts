8957abbbed04556c6693d0a7dbd4fc8d379e7052
Varun114x <thampivarun114@gmail.com>
2022-07-08 15:49:03 +0530

    TODO COMPLETED

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import{HttpClientModule} from '@angular/common/http'
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { TodolistComponent } from './todolist/todolist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
